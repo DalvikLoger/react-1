c0a886a464b77993121ceafa3c59e711e6f6d6f2
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2023-10-20 16:23:06 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+        <Logo />
+        <Navigation />
+        <h1>A Propos</h1><p>lorem100</p>    
+        <br />
+        <p>lorem100</p>
+        </div>
+    );
+};
+
+export default About;
